841b74e436362b231ddd8b686d944a3bf91abef2
emre28 <m.emre1996@outlook.com>
2015-12-16 12:45:02 +0200

    main clası olusturtuldu reis

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-
+import javax.swing.JFrame;
 public class Main {
 
     public static void main(String[] args){
-
+    	JFrame veke= new JFrame("VEKE");
+    	veke.add(new panel_ve_thread);
+    	veke.setResizable(false);//otomatik boyut kapatma
+    	veke.pack();
+    	veke.setLocationRelativeTo(null);//ekrana ortalama
+    	veke.setVisible(true);
+    	veke.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
     }
 }
